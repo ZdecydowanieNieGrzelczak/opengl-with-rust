335237c0e15f9fa514f197161f1c2e26bfe4ee12
Maciek <mgrzelczak97@gmail.com>
2022-04-17 20:06:54 +0200

    Initialization of the small GL window

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+extern crate sdl2;
+extern crate gl;
+
+
+
+fn main() {
+
+
+    let _sdl = sdl2::init().unwrap();
+    let video_subsystem = _sdl.video().unwrap();
+    let window = video_subsystem
+        .window("Game", 900, 700)
+        .opengl()
+        .resizable()
+        .build()
+        .unwrap();
+    let gl_context = window.gl_create_context().unwrap();
+    let gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+
+    unsafe {
+        gl::ClearColor(0.3, 0.3, 0.5, 1.0);
+    }
+    
+
+    let mut event_pump = _sdl.event_pump().unwrap();
+
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit {..} => break 'main,
+                sdl2::event::Event::KeyDown {keycode: key, ..} => {
+                    match key {
+                        Some(sdl2::keyboard::Keycode::Escape) => break 'main,
+                        _ => {}
+                    }
+                },
+                _ => {},
+            }
+        }
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+        
+        window.gl_swap_window();
+
+    }
+}
